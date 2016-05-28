b1c97758551bd4ef609e842625687bed67928a08
alyakhtar <samurai.aly@gmail.com>
2016-05-28 21:16:20 +0530

    version change
    Signed-off-by: alyakhtar <samurai.aly@gmail.com>

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'katastrophe',
   packages = ['katastrophe'], 
-  version = '0.1',
+  version = '0.2',
   description = 'Download torrents from kat.ph directly through terminal',
   author = 'Aly Akhtar',
   author_email = 'samurai.aly@gmail.com',
   url = 'https://github.com/alyakhtar/katastrophe', 
-  download_url = 'https://github.com/alyakhtar/mypackage/tarball/0.1',
+  # download_url = 'https://github.com/alyakhtar/mypackage/tarball/0.1',
   entry_points='''
                [console_scripts]
                katastrophe=katastrophe:main
