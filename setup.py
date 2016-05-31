f144da8bd848af9189d57205ac1ae706747ba14f
Johnathan Jenkins <john@nixheads.co.uk>
2016-05-31 02:56:18 -0700

    linting for setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 from setuptools import setup
-setup(
-  name = 'katastrophe',
-  packages = ['katastrophe'], 
-  version = '1.1.3',
-  description = 'Download torrents from kat.ph directly through terminal',
-  author = 'Aly Akhtar',
-  license = 'MIT',
-  author_email = 'samurai.aly@gmail.com',
-  url = 'https://github.com/alyakhtar/katastrophe', 
-  # download_url = 'https://github.com/alyakhtar/mypackage/tarball/0.1',
-  entry_points='''
+setup(name='katastrophe',
+      packages=['katastrophe'],
+      version='1.1.3',
+      description='Download torrents from kat.ph directly through terminal',
+      author='Aly Akhtar',
+      license='MIT',
+      author_email='samurai.aly@gmail.com',
+      url='https://github.com/alyakhtar/katastrophe',
+      # download_url = 'https://github.com/alyakhtar/mypackage/tarball/0.1',
+      entry_points='''
                [console_scripts]
                katastrophe=katastrophe:main
            ''',
-  install_requires = [
-          'beautifulsoup4',
-          'tabulate',
-          'requests',
-          'lxml',
-          'docopt'
-          ],
-  keywords = ['torrent', 'download', 'kat.ph'],
-)
+      install_requires=[
+          'beautifulsoup4', 'tabulate', 'requests', 'lxml', 'docopt'
+      ],
+      keywords=['torrent', 'download', 'kat.ph'], )
